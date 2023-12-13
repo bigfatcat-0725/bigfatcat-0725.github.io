8b91270821190bd0fccf2112346c7394875f7843
bigfatcat0725 <bigfatcat0725@gmail.com>
2023-12-14 00:13:01 +0900

    123

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
 <div id="frame">
 	<h1>환영합니다.</h1>
 	<h2>필요한 이것저것을 제공중입니다.</h2>
 	<ul>
-		<a href="1rm-calculator"><li>1RM 계산기</li></a>
+		<a href="{base}/1rm-calculator"><li>1RM 계산기</li></a>
 	</ul>
 </div>
 
