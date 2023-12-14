9b27381f68fd5185819a30ad61d2f4eb250f6a39
bigfatcat0725 <bigfatcat0725@gmail.com>
2023-12-14 09:08:29 +0900

    123

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <svelte:head>
-	<link rel="alternate" type="application/rss+xml" title="RSS" href="rss.xml" />
-	<link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml" />
+	<!-- <link rel="alternate" type="application/rss+xml" title="RSS" href="rss.xml" /> -->
+	<!-- <link rel="sitemap" type="application/xml" title="Sitemap" href="sitemap.xml" /> -->
 </svelte:head>
 
 <slot />
